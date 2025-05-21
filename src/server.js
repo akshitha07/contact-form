0fde988ff0b0c2a163298541efb53deb257c49f1
akshitha07 <96717719+akshitha07@users.noreply.github.com>
2025-05-22 04:02:51 +0530

    upadate

--- src/server.js
+++ src/server.js
@@ -4,16 +4,20 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
-const PORT = 5000;
+
 
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
+app.use(express.json())
 
 // Debug log
 console.log('Mongo URI:', process.env.MONGO_URL);
+
+app.use(express.static(path.join(__dirname, '../public')));
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -43,6 +47,8 @@ app.post('/submit', async (req, res) => {
     res.status(500).json({ message: 'Server error' });
   }
 });
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`🚀 Server is running on http://localhost:${PORT}`);
